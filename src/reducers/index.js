fbb61f005786837f239baa6b9a9e2debe992ef43
Kashan724 <112327092+Kashan724@users.noreply.github.com>
2023-04-18 19:46:29 +0500

    adding files

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import changeNumber from "./upDown";
+
+import {combineReducers} from "redux";
+
+// storing reducers 
+const rootReducer =  combineReducers({
+    changeNumber
+
+});
+export default rootReducer;
